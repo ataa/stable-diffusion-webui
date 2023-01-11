76a1f4fce6c73843c4ebaa7d33304e86cf7351b8
Ataa <asgari@gmail.com>
2023-01-11 21:01:03 +0400

    Extra CMD ARGs

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,8 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS=--listen --xformers --enable-insecure-extension-access --opt-channelslast
+REM set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:496
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:464
+git pull
 call webui.bat
