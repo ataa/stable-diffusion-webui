86679a02f39fc02401b42eeaa60405f81df262f4
Ataa <asgari@gmail.com>
2023-01-12 00:59:15 +0400

    Added SAFETENSORS_FAST_GPU=1

--- webui-user.bat
+++ webui-user.bat
@@ -6,5 +6,6 @@ set VENV_DIR=
 set COMMANDLINE_ARGS=--listen --xformers --enable-insecure-extension-access --opt-channelslast
 REM set PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:496
 set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:464
+set SAFETENSORS_FAST_GPU=1
 git pull
 call webui.bat
